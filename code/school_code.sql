fccd19d60aef69131a908efeb4eb9a74075a9779
manikantat407 <141712740+manikantat407@users.noreply.github.com>
2023-09-28 18:32:13 +0530

    Create school_code.sql

---
+++ code/school_code.sql
@@ -0,0 +1,9 @@
+create database Mani;
+use database Mani;
+create schema Arya_schema;
+create table if not exists Mani.Ariya_schema.school(stuid int, sname string, fee int, dptid int);
+insert into  Mani.Ariya_schema.school values (001,"arya",1000,20);
+(005,"srinu',4000,10);
+(003,"suma",3000,05);
+(007,"venky",9000,09);
+
